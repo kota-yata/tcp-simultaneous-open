7bd431c15feb516fb1a406df20aed3d3ae8acb6d
kota-yata <kota@yatagai.com>
2022-10-21 23:48:01 +0900

    Probably stuff before connection all work fine

--- main.c
+++ main.c
@@ -7,34 +7,34 @@
 
 #define BUF_SIZE 1024
 
+int construct_sockaddr(struct sockaddr_in* addr_struct, char* addr, char* port) {
+  memset(addr_struct, 0, sizeof(*addr_struct));
+  addr_struct->sin_family = AF_INET;
+  addr_struct->sin_addr.s_addr = inet_addr(addr);
+  addr_struct->sin_port = htons(atoi(port));
+  return 0;
+}
+
 int main(int argc, char *argv[]) {
   char* message = "Hello :)";
-  char* buffer = malloc(BUF_SIZE);
-  int fd_in;
-  int fd_out;
-  int fd_misc;
+  int fd_in = -1;
+  int fd_out = -1;
+  int fd_misc = -1;
 
-  int m_socket = socket(AF_INET, SOCK_STREAM, 0);
-  if (m_socket < 0) {
+  fd_in = socket(AF_INET, SOCK_STREAM, 0);
+  if (fd_in < 0) {
     printf("Socket generation failed\n");
     return -1;
   }
 
   struct sockaddr_in my_addr;
-  memset(&my_addr, 0, sizeof(my_addr));
-  my_addr.sin_family = AF_INET;
-  my_addr.sin_addr.s_addr = inet_addr(argv[1]);
-  my_addr.sin_port = htons(atoi(argv[2]));
-
+  construct_sockaddr(&my_addr, argv[1], argv[2]);
   struct sockaddr_in peer_addr;
-  memset(&peer_addr, 0, sizeof(peer_addr));
-  peer_addr.sin_family = AF_INET;
-  peer_addr.sin_addr.s_addr = inet_addr(argv[3]);
-  peer_addr.sin_port = htons(atoi(argv[4]));
+  construct_sockaddr(&my_addr, argv[3], argv[4]);
 
   int bind_res = bind(fd_in, (struct sockaddr*) &my_addr, sizeof(my_addr));
   if (bind_res < 0) {
-    printf("Failed to bind");
+    printf("Failed to bind\n");
     close(fd_in);
     return -1;
   }
@@ -47,11 +47,10 @@ int main(int argc, char *argv[]) {
   }
 
   send(0, message, strlen(message), 0);
+  char* buffer = malloc(BUF_SIZE);
   ssize_t received_val = recv(fd_misc, buffer, BUF_SIZE, 0);
   printf("%s\n", buffer);
-
   free(buffer);
-
   close(fd_in);
 
   return 0;

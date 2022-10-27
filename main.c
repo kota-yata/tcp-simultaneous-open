7bf8e58428140974295a7b5bceda7f2821a172f4
kota-yata <kota@yatagai.com>
2022-10-27 14:27:55 +0900

    WIP

--- main.c
+++ main.c
@@ -25,10 +25,11 @@ int get_remaining_sec() {
 
 int main(int argc, char *argv[]) {
   char* message = "Hello :)";
+  int fd_out = -1;
   int fd_in = -1;
 
-  fd_in = socket(AF_INET, SOCK_STREAM, 0);
-  if (fd_in < 0) {
+  fd_out = socket(AF_INET, SOCK_STREAM, 0);
+  if (fd_out < 0) {
     printf("Socket generation failed\n");
     return -1;
   }
@@ -38,10 +39,9 @@ int main(int argc, char *argv[]) {
   struct sockaddr_in peer_addr;
   construct_sockaddr(&peer_addr, argv[3], argv[4]);
 
-  int bind_res = bind(fd_in, (struct sockaddr*) &my_addr, sizeof(my_addr));
-  if (bind_res < 0) {
+  if (bind(fd_out, (struct sockaddr*) &my_addr, sizeof(my_addr)) < 0) {
     printf("Failed to bind %s\n", argv[1]);
-    close(fd_in);
+    close(fd_out);
     return -1;
   }
 
@@ -50,17 +50,25 @@ int main(int argc, char *argv[]) {
   sleep(wait_for);
 
   // Connection Attempt
-  int connect_res = connect(fd_in, (struct sockaddr*) &my_addr, sizeof(my_addr));
-  if (connect_res < 0) {
+  if (connect(fd_out, (struct sockaddr*) &peer_addr, sizeof(peer_addr)) < 0) {
     printf("Connection attempt failed\n");
     return - 1;
   }
+  printf("Connected!");
 
-  write(fd_in, message, sizeof(message));
+  // write(fd_out, message, sizeof(message));
+  if (send(fd_out, message, sizeof(message), 0) < 0) {
+    printf("Failed to send message");
+    return -1;
+  }
   char* buffer = malloc(BUF_SIZE);
-  int read_res = read(fd_in, buffer, BUF_SIZE);
-  fprintf(stderr, "Received \"%.*s\" from socket\n", read_res, buffer);
+  if (read(fd_in, buffer, BUF_SIZE) < 0) {
+    printf("Failed to read message");
+    return -1;
+  }
+  printf("Received: %s \n", buffer);
   free(buffer);
+  close(fd_out);
   close(fd_in);
 
   return 0;

4e3b9f96f648b0373d6e9a2c6d02ba1a7c40b4c8
kota-yata <kota@yatagai.com>
2022-10-31 13:53:37 +0900

    Modify client code

--- src/simple-stun/client.c
+++ src/simple-stun/client.c
@@ -8,8 +8,9 @@
 int main(int argc, char *argv[]) {
   int descriptor = -1;
 
-  struct sockaddr_in sin;
+  struct sockaddr_in sin_server, sin;
   memset(&sin, 0, sizeof(sin));
+  memset(&sin_server, 0, sizeof(sin_server));
 
   descriptor = socket(AF_INET, SOCK_STREAM, 0);
   if (descriptor < 0) {
@@ -18,11 +19,21 @@ int main(int argc, char *argv[]) {
   }
 
   sin.sin_family = AF_INET;
-  sin.sin_addr.s_addr = inet_addr(argv[1]);
-  sin.sin_port = htons(atoi(argv[2]));
+  sin.sin_addr.s_addr = inet_addr("0.0.0.0");
+  sin.sin_port = htons(atoi(argv[1]));
 
-  if (connect(descriptor, (const struct sockaddr *)&sin, sizeof(sin)) < 0) {
-    printf("Failed to connect");
+  if (bind(descriptor, (struct sockaddr *)&sin, sizeof(sin)) < 0) {
+    printf("Failed to bind");
+    return -1;
+  };
+
+  sin_server.sin_family = AF_INET;
+  sin_server.sin_addr.s_addr = inet_addr(argv[2]);
+  sin_server.sin_port = htons(atoi(argv[3]));
+  
+
+  if (connect(descriptor, (const struct sockaddr *)&sin_server, sizeof(sin_server)) < 0) {
+    printf("Failed to connect\n");
     close(descriptor);
     return -1;
   }
